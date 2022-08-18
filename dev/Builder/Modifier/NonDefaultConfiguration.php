4d24a6704b50e2a7a6052efd00a126249a0a539d
Kuba Werłos <werlos@gmail.com>
2022-08-18 22:36:41 +0200

    Fix

--- dev/Builder/Modifier/NonDefaultConfiguration.php
+++ dev/Builder/Modifier/NonDefaultConfiguration.php
@@ -38,8 +38,8 @@ final class NonDefaultConfiguration
         $rules['no_superfluous_phpdoc_tags'] = ['remove_inheritdoc' => true];
         $rules['php_unit_test_case_static_method_calls'] = ['call_type' => 'self'];
         $rules['phpdoc_line_span'] = ['property' => 'single'];
-        $rules['whitespace_after_comma_in_array'] = ['ensure_single_space' => true];
         $rules['trailing_comma_in_multiline'] = ['after_heredoc' => true, 'elements' => ['arguments', 'arrays', 'parameters']];
+        $rules['whitespace_after_comma_in_array'] = ['ensure_single_space' => true];
         $rules['yoda_style'] = ['equal' => false, 'identical' => false, 'less_and_greater' => false];
 
         return $rules;
